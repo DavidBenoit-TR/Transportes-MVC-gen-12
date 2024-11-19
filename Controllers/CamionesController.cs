7a334284f3dba052dac75ed7808f2ef54834aa51
David Benoit <mdavid.benoit@gmail.com>
2024-11-19 11:01:22 -0600

    Camiones: Insertar Terminado

--- Controllers/CamionesController.cs
+++ Controllers/CamionesController.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using DTO;
+using System;
 using System.Collections.Generic;
 using System.Diagnostics.CodeAnalysis;
+using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -44,5 +46,99 @@ namespace Transportes_MVC_gen_12.Controllers
             //retorno la vista con los datos del modelo
             return View(lista_camiones);
         }
+
+        //GET: Nuevo_Camion
+        public ActionResult Nuevo_Camion()
+        {
+            ViewBag.Titulo = "Nuevo Camión";
+            //cargar DLL con las opciones del tipo de camión
+            cargarDDL();
+            return View();
+        }
+
+        //POST: Nuevo_Camion
+        [HttpPost]
+        public ActionResult Nuevo_Camion(Camiones_DTO model, HttpPostedFileBase imagen)
+        {
+            try
+            {
+                if (ModelState.IsValid) //Verificar si los campos y los tipos de datos corresponden al Modelo (DTO)
+                {
+                    using (TransportesEntities context = new TransportesEntities()) //creo una unstancia de un solo uso de mi contexto
+                    {
+                        //creo un objeto basado en el contexto (Modelo Original)
+                        var camion = new Camiones();
+
+                        //asigno todos los valores del modelo de entrada (DTO) el objeto que será insertado en la BD (modelo original)
+                        camion.Matricula = model.Matricula;
+                        camion.Marca = model.Marca;
+                        camion.Modelo = model.Modelo;
+                        camion.Capacidad = model.Capacidad;
+                        camion.Kilometraje = model.Kilometraje;
+                        camion.Disponibilidad = model.Disponibilidad;
+                        camion.Tipo_Camion = model.Tipo_Camion;
+
+                        //valido si existe una IMG o no
+                        if (imagen != null && imagen.ContentLength > 0)
+                        {
+                            string filename = Path.GetFileName(imagen.FileName); //recupero el nombre de mi archivo
+                            string pathdir = Server.MapPath("~/Assets/Imagenes/Camiones/"); //mapeo la ruta donde guardaré mis imágenes en el servidor
+                            if (!Directory.Exists(pathdir)) //si no existe el directorio, lo creo
+                            {
+                                Directory.CreateDirectory(pathdir);
+                            }
+                            imagen.SaveAs(pathdir + filename); //guardo la imagen en el servidor
+                            camion.UrlFoto = "/Assets/Imagenes/Camiones/" + filename; //guardo la ruta y el nombre del archivo en el camión que voy a insertar
+
+                            //Impacto sobre la BD usando EF
+                            context.Camiones.Add(camion); //Agrego un nuevo caión al contexto
+                            context.SaveChanges(); //impactar la base de datos (enviár los cambios entre el contexto y la BD)
+                            //Sweet Alert
+                            return RedirectToAction("Index"); //finalmente, regreso al listado si es que todo salió bien
+                        }
+                        else
+                        {
+                            //sweet alert
+                            cargarDDL();
+                            return View(model);
+                        }
+                    }
+                }
+                else
+                {
+                    //En caso de que ocurra alguna exepción, voy a mostrar un msj con el error, voy a volver a cargar la lista de opciones del tipo_camion (cargarDDL) par que no marque error el formulario, y devulveré la misma vista con los mismos datos que me han sido enviado (vas pa' tras)
+                    //Sweet Alert
+                    cargarDDL();
+                    return View(model);
+                }
+            }
+            catch (Exception ex)
+            {
+                //En caso de que ocurra alguna exepción, voy a mostrar un msj con el error, voy a volver a cargar la lista de opciones del tipo_camion (cargarDDL) par que no marque error el formulario, y devulveré la misma vista con los mismos datos que me han sido enviado (vas pa' tras)
+                //Sweet Alert
+                cargarDDL();
+                return View(model);
+            }
+        }
+
+        #region Auxiliares
+        private class Opciones
+        {
+            public string Numero { get; set; }
+            public string Descripcion { get; set; }
+        }
+
+        public void cargarDDL()
+        {
+            List<Opciones> lista_opciones = new List<Opciones>() {
+                new Opciones() {Numero = "0", Descripcion="Seleccione una opción"},
+                new Opciones() {Numero = "1", Descripcion="Volteo"},
+                new Opciones() {Numero = "2", Descripcion="Redilas"},
+                new Opciones() {Numero = "3", Descripcion="Transporte"}
+            };
+
+            ViewBag.ListaTipos = lista_opciones;
+        }
+        #endregion
     }
 }
